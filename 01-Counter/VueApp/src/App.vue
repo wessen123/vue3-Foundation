2bd8d3bb561b3b0f68ffa3d9c5174929736c2398
wondwessen <wessen333@gmail.com>
2023-12-25 04:13:46 -0800

    first commit

---
+++ 01-Counter/VueApp/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref} from "vue"
+
+let count = ref(0)
+</script>
+
+<template>
+  <main>
+      <container class="container">
+        <h4>the current count is...</h4>
+        <h1>{{count}}</h1>
+        <button @click="count++">+</button>
+        <button @click="count--">-</button>
+      </container>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  background-color: aliceblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  text-align: center;
+}
+</style>
